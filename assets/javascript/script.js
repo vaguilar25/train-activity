28e087d6181e6dff5844bc6e08649d0f0b22f11e
vaguilar25 <vivian.aguilar@gmail.com>
2018-11-28 17:33:56 -0600

    Initial File .. a copy of another one.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -56,96 +56,45 @@ function time_convert(num) {
 
 
 database.ref().on('child_added', function (snapshot) {
-    console.log("Snapshot: " + snapshot.key);
+    
     var newPost = snapshot.val();
-    console.log("New Post: " + snapshot.key);
-
-    var today = new Date();
-    var a = moment([120]);
-    var b = moment([1320]);
-    var differences = a.diff(b, 'hours') // 1
-
-    var format = 'hh:mm:ss'
-    var time = moment("1:05", format);
-    console.log("Now: " + time);
-
-
-    var date1 = moment('08/10/2016', 'MM/DD/YYYY'),
-        date2 = moment('08/15/2016', 'MM/DD/YYYY');
-
-    var duration = moment.duration(date2.diff(date1));
-    duration = duration.asHours();
-
-    console.log("Duration as Hours" + duration);
-
-
-    var a = moment([2007, 0, 28]);
-    var b = moment([2007, 0, 29]);
-    a.from(b) // "a day ago"
-
-    console.log("difference between a and b" + a.from(b));
-
-    console.log("TIme Convert: " + time_convert(100));
-    console.log("Duration:" + differences);
-
-
-    var toNow = moment([2007, 0, 29],"hh").toNow(true);
-
-    console.log ("Train Time:" + "2010-10-20 " + newPost.trainTime );
-    console.log("Momen is valid:" + moment(newPost.trainTime , 'HH:mm').isValid());
-
-    // console.log("Prev Child Key: " + prevChildKey);
-    //console.log(moment(newPost.trainTime).format("hh:mm A"));
-
+    //Get the Train Time
     var trainTime = moment(newPost.trainTime , "HH:mm").format("HH:mm a");
 
-    console.log("Train Time" + trainTime);
+   //Get the current Time
     var currentTime = moment().format("HH:mm a");
-   // var formatCurrent = moment(currentTime,"HH:mm");
-    console.log("TrainTime" + trainTime);
-    console.log("formatCurrent: " + currentTime);
-    console.log("Difference between Times: "  + moment.duration(trainTime).humanize());
-
-
+   
+    
+    //Get the difference from now and train time
     var timeDifference = moment().diff(moment(newPost.trainTime , "HH:mm"), "minutes");
-    console.log("timeDifference" + timeDifference);
-    var timeDivision = timeDifference%newPost.frequency;
-    console.log("T reminder " + timeDivision);
-    var minutesDifference = newPost.frequency - timeDivision;
-    console.log("minutes Reminder " + minutesDifference);
-    var nextTRain = moment().add(minutesDifference, "minutes").format ("hh:mm A");
-    console.log("next Train " + nextTRain);
+  
 
+    //Time left
+    var timeDivision = timeDifference%newPost.frequency;
+     var minutesDifference = newPost.frequency - timeDivision;
+   
+    //Next train 
+    var nextTRain = moment().add(minutesDifference, "minutes").format ("hh:mm A");
     var trainTime = moment(newPost.trainTime, "HH:mm");
-    console.log("First Time: " + moment(newPost.trainTime, "HH:mm"));
+   
     var currentTime = moment();
 
     var differenceInTimes = moment(trainTime).diff(currentTime, "minutes");
-    console.log("differenceInTimes " + differenceInTimes);
-     var minutesLeft = Math.ceil(moment.duration(differenceInTimes).asMinutes());
-
-     console.log("Difference: " + moment.duration(differenceInTimes).asMinutes());
-
-     console.log("Math Ceil: " + Math.ceil(moment.duration(differenceInTimes).asMinutes()) + "Round: " + Math.round(moment.duration(differenceInTimes).asMinutes()) );
-
-     console.log("Before Minutes" + minutesLeft);
-
+   
+    
      if ((currentTime - trainTime) < 0) {
         nextTrain = snapshot.val().firstTime;
-        console.log("Before First Train");
-        minutesDifference = minutesLeft;
+  
+        minutesDifference = Math.round(moment.duration(differenceInTimes).asMinutes());;
       }
       else {
         nextTrain = moment().add(minutesDifference, "minutes").format("hh:mm A");
         minutesDifference = newPost.frequency - timeDivision;
-        console.log("Working");
+   
       }
 
 
-    $(".results").append("<tr><td>" + newPost.trainName + "</td>" + "<td>" + newPost.destination + "</td><td>" + newPost.frequency + "</td><td>" + nextTRain + "</td><td>" + newPost.trainTime + "</td><td> </td><td> <button> Update </button>    </td><td> <button class='delete' id=" + snapshot.key + "> Delete </button>    </td></tr>");
-
-
-    // postDom(newPost, snapshot);
+    $(".results").append("<tr><td>" + newPost.trainName + "</td>" + "<td>" + newPost.destination + "</td><td>" + newPost.frequency + "</td><td>" + nextTRain + "</td><td>" + minutesDifference + "</td><td> </td><td> <button class='delete' id=" + snapshot.key + "> Delete </button>    </td></tr>");
 
 
 }, function (errorObject) {
@@ -158,7 +107,7 @@ $(document).on("click", ".delete", function () {
     console.log("id: " + id);
 
     database.ref().child(id).remove();
-    //  database().ref().update();
+    
 
 });
 
